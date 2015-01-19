05755cffcaccd4cd21b372894fd8e14feca98865
markmohr15 <mark@interfaceschool.com>
2015-01-19 12:01:21 -0600

    Install Active Admin

---
+++ db/migrate/20150119175836_drop_active_admin_comments_and_admin_users.rb
@@ -0,0 +1,6 @@
+class DropActiveAdminCommentsAndAdminUsers < ActiveRecord::Migration
+  def change
+    drop_table :active_admin_comments
+    drop_table :admin_users
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150119172022) do
+ActiveRecord::Schema.define(version: 20150119175836) do
 
   create_table "players", force: :cascade do |t|
     t.integer  "sport_id",   limit: 4
