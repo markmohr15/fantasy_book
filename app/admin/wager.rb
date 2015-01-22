2c7f08d22d34bfed5c7bbb3d963ed7557456d167
markmohr15 <mark@interfaceschool.com>
2015-01-22 02:37:31 -0600

    Clean up AA for Wager

--- app/admin/wager.rb
+++ app/admin/wager.rb
@@ -2,17 +2,19 @@ ActiveAdmin.register Wager do
 
   filter :id
   filter :user
-  filter :props
-  filter :state
-  filter :risk
+  filter :prop_id
+  filter :state, label: "Status", as: :select, collection: Wager.states
+  #custom risk filter
 
   index do
     selectable_column
     column :id
     column :user
     column :prop
-    column :state
-    column :risk_dollars
+    column "Status", :state do |wager|
+      wager.aasm_current_state
+    end
+    column "Risk", :risk_dollars
     actions
   end
 
@@ -21,10 +23,15 @@ ActiveAdmin.register Wager do
       row :id
       row :user
       row :prop
-      row :state
-      row :risk_dollars
-      row :win_dollars
-      row :state
+      row "Status", :state do |wager|
+        wager.aasm_current_state
+      end
+      row "Risk" do
+        wager.risk_dollars
+      end
+      row "Win" do
+        wager.win_dollars
+      end
       row "Pick" do |wager|
         if wager.pick == "away"
           wager.prop.player1.name + " & " + wager.prop.player2.name
@@ -32,8 +39,12 @@ ActiveAdmin.register Wager do
           wager.prop.player3.name + " & " + wager.prop.player4.name
         end
       end
-      row :spread_line
-      row :vig
+      row "Spread" do
+        wager.spread_line
+      end
+      row "Vig" do
+        wager.vig_juice
+      end
       row :created_at
       row :updated_at
     end
@@ -43,7 +54,7 @@ ActiveAdmin.register Wager do
     f.inputs "Wager Details" do
       f.input :user
       f.input :prop_id
-      f.input :state
+      f.input :state, label: "Status", as: :select, collection: f.object.aasm.states.map(&:name), include_blank: false
       f.input :risk_dollars, label: "Risk"
       f.input :win_dollars, label: "Win"
       f.input :pick, as: :select, collection: ["away", "home"]
