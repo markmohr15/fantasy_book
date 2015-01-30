38446936350cdcce9fc24ae653b2fcaaa55e234e
markmohr15 <mark@interfaceschool.com>
2015-01-30 12:23:14 -0600

    Move AA JS to its own file and tell heroku to precompile it

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,37 +16,4 @@
 //= require turbolinks
 //= require_tree .
 
-$(document).ready(function() {
 
-  document.getElementById("new_prop").elements.namedItem("prop_variety").onchange = function () {
-  if (document.getElementById("new_prop").elements.namedItem("prop_variety").value == "PvP") {
-    document.getElementById("prop_prop_choices_attributes_0_choice_raw_input").remove()
-    document.getElementById("prop_prop_choices_attributes_1_choice_raw_input").remove()
-    document.getElementById("prop_prop_choices_attributes_0_player2_input").remove()
-    document.getElementById("prop_prop_choices_attributes_1_player2_input").remove()
-    document.getElementById("prop_over_under_input").remove()
-    $('.has_many_add').remove()
-} else if (document.getElementById("new_prop").elements.namedItem("prop_variety").value == "2Pv2P") {
-    document.getElementById("prop_prop_choices_attributes_0_choice_raw_input").remove()
-    document.getElementById("prop_prop_choices_attributes_1_choice_raw_input").remove()
-    document.getElementById("prop_over_under_input").remove()
-    $('.has_many_add').remove()
-} else if (document.getElementById("new_prop").elements.namedItem("prop_variety").value == "Over/Under") {
-    document.getElementById("prop_prop_choices_attributes_0_player1_input").remove()
-    document.getElementById("prop_prop_choices_attributes_1_player1_input").remove()
-    document.getElementById("prop_prop_choices_attributes_0_player2_input").remove()
-    document.getElementById("prop_prop_choices_attributes_1_player2_input").remove()
-    document.getElementById("prop_opt1_spread_input").remove()
-    $('.has_many_add').remove()
-    document.getElementById("prop_prop_choices_attributes_0_choice_raw").value = "Over"
-    document.getElementById("prop_prop_choices_attributes_1_choice_raw").value = "Under"
-} else if (document.getElementById("new_prop").elements.namedItem("prop_variety").value == "Other") {
-    document.getElementById("prop_prop_choices_attributes_0_player1_input").remove()
-    document.getElementById("prop_prop_choices_attributes_1_player1_input").remove()
-    document.getElementById("prop_prop_choices_attributes_0_player2_input").remove()
-    document.getElementById("prop_prop_choices_attributes_1_player2_input").remove()
-    document.getElementById("prop_opt1_spread_input").remove()
-}
-}
-
-});
