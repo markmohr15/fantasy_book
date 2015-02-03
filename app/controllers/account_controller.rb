f23558d51454235695ab0c337d5be2ded0afb47a
markmohr15 <mark@interfaceschool.com>
2015-02-03 02:03:19 -0600

    Retool main nav, create my_account nav, and corresponding pages

---
+++ app/controllers/account_controller.rb
@@ -0,0 +1,14 @@
+class AccountController < ApplicationController
+  def deposit
+    render
+  end
+
+  def withdraw
+    render
+  end
+
+  def transfer
+    render
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get "/contact_us", to: "pages#contact"
   get "/privacy_policy", to: "pages#privacy"
   get "/rules", to: "pages#rules"
+  get "/my_account/deposit", to: "account#deposit"
+  get "/my_account/withdraw", to: "account#withdraw"
+  get "/my_account/transfer", to: "account#transfer"
 
   root to: "pages#home"
 end
