be7c2a3ad4468766f3b8423b6a092858e455e24f
markmohr15 <mark@interfaceschool.com>
2015-02-11 17:07:14 -0600

    format date from datepicker

--- app/controllers/fantasy_controller.rb
+++ app/controllers/fantasy_controller.rb
@@ -9,11 +9,11 @@ class FantasyController < ApplicationController
 
   def my_history
     if params[:beg_date].present?
-      #start_params = params[:beg_date]
-      #ending_params = params[:end_date]
-      #start_date = DateTime.new(start_params["year"].to_i, start_params["month"].to_i, start_params["day"].to_i)
-      #ending_date = DateTime.new(ending_params["year"].to_i, ending_params["month"].to_i, ending_params["day"].to_i)
-      @wagers = Wager.joins(:prop).where('props.time' => params[:beg_date]..params[:end_date],
+      start_params = params[:beg_date]
+      ending_params = params[:end_date]
+      start_date = start_params[6..9] + start_params[5] + start_params[0..2] + start_params[3..4]
+      ending_date = ending_params[6..9] + ending_params[5] + ending_params[0..2] + ending_params[3..4]
+      @wagers = Wager.joins(:prop).where('props.time' => start_date..ending_date,
       'state' => 1..3, 'user_id' => current_user.id)
     else
       @wagers = Wager.where(state: 1..3, user_id: current_user.id)
