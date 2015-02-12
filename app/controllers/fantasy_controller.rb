704c0e82fa73bfd8269944550c2e10e2be05efac
markmohr15 <mark@interfaceschool.com>
2015-02-11 18:26:13 -0600

    Add time of day to datepicker params

--- app/controllers/fantasy_controller.rb
+++ app/controllers/fantasy_controller.rb
@@ -11,8 +11,8 @@ class FantasyController < ApplicationController
     if params[:beg_date].present?
       start_params = params[:beg_date]
       ending_params = params[:end_date]
-      start_date = start_params[6..9] + start_params[5] + start_params[0..2] + start_params[3..4]
-      ending_date = ending_params[6..9] + ending_params[5] + ending_params[0..2] + ending_params[3..4]
+      start_date = start_params[6..9] + start_params[5] + start_params[0..2] + start_params[3..4] + " 00:00:00"
+      ending_date = ending_params[6..9] + ending_params[5] + ending_params[0..2] + ending_params[3..4] + " 23:59:59"
       @wagers = Wager.joins(:prop).where('props.time' => start_date..ending_date,
       'state' => 1..3, 'user_id' => current_user.id)
     else
