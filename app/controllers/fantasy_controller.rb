374e4fa8742bbc2c96fe6065e3047b63217e6c2f
markmohr15 <mark@interfaceschool.com>
2015-02-12 23:32:10 -0600

    Set my_history default to current week

--- app/controllers/fantasy_controller.rb
+++ app/controllers/fantasy_controller.rb
@@ -13,11 +13,12 @@ class FantasyController < ApplicationController
       ending_params = params[:end_date]
       start_date = start_params[6..9] + start_params[5] + start_params[0..2] + start_params[3..4] + " 00:00:00"
       ending_date = ending_params[6..9] + ending_params[5] + ending_params[0..2] + ending_params[3..4] + " 23:59:59"
-      @wagers = Wager.joins(:prop).where('props.time' => start_date..ending_date,
-      'state' => 1..3, 'user_id' => current_user.id)
     else
-      @wagers = Wager.where(state: 1..3, user_id: current_user.id)
+      start_date = Date.today.beginning_of_week(:monday)
+      ending_date = Time.now
     end
+    @wagers = Wager.joins(:prop).where('props.time' => start_date..ending_date,
+      'state' => 1..3, 'user_id' => current_user.id)
   end
 
   def my_stats
