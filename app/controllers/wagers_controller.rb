46d78ce9a438e215138fd4b891ff9e691cdc44a0
markmohr15 <mark@interfaceschool.com>
2015-02-16 11:58:44 -0600

    Wager Test

--- app/controllers/wagers_controller.rb
+++ app/controllers/wagers_controller.rb
@@ -2,7 +2,19 @@ class WagersController < ApplicationController
   layout "application"
 
   def create_multiple
+    params[:wager].each do |attr|
+      wager = Wager.new(attr)
+      wager.user_id = current_user.id
+      wager.save
+    end
 
+    redirect_to root_path
+  end
+
+  private
+
+  def wager_params
+    params.require(:wager).permit(:prop_id, :prop_choice_id, :risk, :odds_juice, :spread_line, :total)
   end
 
 end
