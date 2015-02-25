b6c16c855bc5d66b2498147af346e2d596440484
markmohr15 <mark@interfaceschool.com>
2015-02-25 11:56:30 -0600

    Default my_histroy set to last 7 days

--- app/controllers/fantasy_controller.rb
+++ app/controllers/fantasy_controller.rb
@@ -9,15 +9,14 @@ class FantasyController < ApplicationController
   end
 
   def my_history
-    if params[:beg_date].present?
-      start_params = params[:beg_date]
-      ending_params = params[:end_date]
-      start_date = start_params[6..9] + start_params[5] + start_params[0..2] + start_params[3..4] + " 00:00:00"
-      ending_date = ending_params[6..9] + ending_params[5] + ending_params[0..2] + ending_params[3..4] + " 23:59:59"
-    else
-      start_date = Date.today.beginning_of_week(:monday)
-      ending_date = Time.now
+    unless params[:beg_date].present?
+      params[:beg_date] = (DateTime.now - 7.days).strftime("%m/%d/%Y")
+      params[:end_date] = DateTime.now.strftime("%m/%d/%Y")
     end
+    start_params = params[:beg_date]
+    ending_params = params[:end_date]
+    start_date = start_params[6..9] + start_params[5] + start_params[0..2] + start_params[3..4] + " 00:00:00"
+    ending_date = ending_params[6..9] + ending_params[5] + ending_params[0..2] + ending_params[3..4] + " 23:59:59"
     @wagers = Wager.joins(:prop).where('props.time' => start_date..ending_date,
       'state' => 1..3, 'user_id' => current_user.id)
     @result_counter = 0
