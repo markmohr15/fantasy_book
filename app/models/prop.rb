a286d81cb9713774ce6e06c3fb431b1382ec59d6
markmohr15 <mark@interfaceschool.com>
2015-04-03 01:17:32 -0500

    Send mailer on regrade of prop

--- app/models/prop.rb
+++ app/models/prop.rb
@@ -79,6 +79,10 @@ class Prop < ActiveRecord::Base
       self.ungrade_wagers
       self.state = "Closed"
       self.grade_prop!
+      wagers = Wager.where(prop_id: self.id)
+      wagers.each do |wager|
+        MailgunMailer.contest_graded(wager).deliver_later
+      end
     end
   end
 
