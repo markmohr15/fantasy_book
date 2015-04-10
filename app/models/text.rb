1ee36ac02b059e528a21200978434c7ac2b1f795
markmohr15 <mark@interfaceschool.com>
2015-04-10 12:23:21 -0500

    Fix Text receivers

--- app/models/text.rb
+++ app/models/text.rb
@@ -20,7 +20,7 @@ class Text < ActiveRecord::Base
   def self.transfer_request transfer
     text = transfer.sender.username + ", you have requested a transfer of " + sprintf('%.2f', transfer.amount_dollars).to_s + " to " + transfer.receiver.username + ".  We are currently reviewing your request and will let you know of the result as soon as possible.\nThank you,\nFantasyBook.guru"
     client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
-    message = client.messages.create from: "+15406843040", to: user.phone, body: text
+    message = client.messages.create from: "+15406843040", to: transfer.sender.phone, body: text
   end
 
   def self.transfer_approved transfer
@@ -44,19 +44,19 @@ class Text < ActiveRecord::Base
   def self.withdrawal_request withdrawal
     text = withdrawal.user.username + ", you just requested a " + withdrawal.kind + " withdrawal in the amount of " + sprintf('%.2f', withdrawal.net_amount).to_s + ".  Your request will be processed in 24-48 business hours.\nThank you,\nFantasyBook.guru"
     client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
-    message = client.messages.create from: "+15406843040", to: user.phone, body: text
+    message = client.messages.create from: "+15406843040", to: withdrawal.user.phone, body: text
   end
 
   def self.withdrawal_approved withdrawal
     text = withdrawal.user.username + ", your " + withdrawal.kind + " withdrawal request in the amount of " + sprintf('%.2f', withdrawal.net_amount).to_s + "was approved.\nThank you,\nFantasyBook.guru"
     client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
-    message = client.messages.create from: "+15406843040", to: user.phone, body: text
+    message = client.messages.create from: "+15406843040", to: withdrawal.user.phone, body: text
   end
 
   def self.withdrawal_rejected withdrawal
     text = withdrawal.user.username + ", your " + withdrawal.kind + " withdrawal in the amount of " + sprintf('%.2f', withdrawal.net_amount).to_s + "was rejected.  If you need further information, please contact us at info@fantasybook.guru.\nThank you,\nFantasyBook.guru"
     client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
-    message = client.messages.create from: "+15406843040", to: user.phone, body: mass_email.message
+    message = client.messages.create from: "+15406843040", to: withdrawal.user.phone, body: mass_email.message
   end
 
 end
